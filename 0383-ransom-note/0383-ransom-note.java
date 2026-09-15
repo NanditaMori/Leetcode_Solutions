88abe4031f77b70037c57c5310fbc29a0a4c5053
Nandita <nandita11032007@gmail.com>
2026-09-15 21:50:33 +0530

    Time: 1 ms (99.95%), Space: 46.3 MB (79.29%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int [] ans = new int[26];
+        for(int i=0 ; i<magazine.length() ; i++)
+        {
+            ans[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0 ; i<ransomNote.length() ; i++)
+        {
+            ans[ransomNote.charAt(i)-'a']--;
+        }
+        for(int i=0 ; i<ans.length ; i++)
+        {
+            if(ans[i]<0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
